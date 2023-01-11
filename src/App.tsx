3805a1f90265e51086f95c9609f845c80dd1055d
Bhrugu <79759496+Bhrugu14@users.noreply.github.com>
2023-01-11 23:45:25 +0530

    changed design. added devtool for react query

--- src/App.tsx
+++ src/App.tsx
@@ -17,59 +17,64 @@ function App() {
     <div>
       <div>List of Posts</div>
       {status === "loading" && <div>LOADING....</div>}
-      {status === "success" &&
-        data.map(
-          (
-            i: {
-              userId:
-                | string
-                | number
-                | boolean
-                | ReactElement<any, string | JSXElementConstructor<any>>
-                | ReactFragment
-                | ReactPortal
-                | null
-                | undefined;
-              id:
-                | string
-                | number
-                | boolean
-                | ReactElement<any, string | JSXElementConstructor<any>>
-                | ReactFragment
-                | ReactPortal
-                | null
-                | undefined;
-              title:
-                | string
-                | number
-                | boolean
-                | ReactElement<any, string | JSXElementConstructor<any>>
-                | ReactFragment
-                | ReactPortal
-                | null
-                | undefined;
-              completed:
-                | string
-                | number
-                | boolean
-                | ReactElement<any, string | JSXElementConstructor<any>>
-                | ReactFragment
-                | ReactPortal
-                | null
-                | undefined;
-            },
-            k: string
-          ) => {
-            return (
-              <div key={"data" + k} className="flex flex-col">
-                <label>userId: {i.userId}</label>
-                <label>id: {i.id}</label>
-                <label>title: {i.title}</label>
-                <label>completed: {i.completed}</label>
-              </div>
-            );
-          }
-        )}
+      <div className="flex w-full flex-wrap">
+        {status === "success" &&
+          data.map(
+            (
+              i: {
+                userId:
+                  | string
+                  | number
+                  | boolean
+                  | ReactElement<any, string | JSXElementConstructor<any>>
+                  | ReactFragment
+                  | ReactPortal
+                  | null
+                  | undefined;
+                id:
+                  | string
+                  | number
+                  | boolean
+                  | ReactElement<any, string | JSXElementConstructor<any>>
+                  | ReactFragment
+                  | ReactPortal
+                  | null
+                  | undefined;
+                title:
+                  | string
+                  | number
+                  | boolean
+                  | ReactElement<any, string | JSXElementConstructor<any>>
+                  | ReactFragment
+                  | ReactPortal
+                  | null
+                  | undefined;
+                completed:
+                  | string
+                  | number
+                  | boolean
+                  | ReactElement<any, string | JSXElementConstructor<any>>
+                  | ReactFragment
+                  | ReactPortal
+                  | null
+                  | undefined;
+              },
+              k: string
+            ) => {
+              return (
+                <div
+                  key={"data" + k}
+                  className="flex flex-col h-56 w-1/5 border-red-400 border justify-center"
+                >
+                  <label>userId: {i.userId}</label>
+                  <label>id: {i.id}</label>
+                  <label>title: {i.title}</label>
+                  <label>completed: {i.completed}</label>
+                </div>
+              );
+            }
+          )}
+      </div>
     </div>
   );
 }
